b25f4eaeac15be4d8a39d218504b8c7153c4ea09
pablojg9 <pablojuniogn2@gmail.com>
2023-06-11 22:04:53 -0300

    started project

---
+++ src/test/java/br/com/gntech/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gntech.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
