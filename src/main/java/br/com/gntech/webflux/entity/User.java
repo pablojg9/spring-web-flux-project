e7d00e7f17a454ace4760b6504ca46b40ef8eb93
pablojg9 <pablojuniogn2@gmail.com>
2023-06-13 21:00:18 -0300

    add user entity

---
+++ src/main/java/br/com/gntech/webflux/entity/User.java
@@ -0,0 +1,22 @@
+package br.com.gntech.webflux.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+
+}
