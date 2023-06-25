616d0e2db0f42131c09da9d6af90311b8eb78a8f
pablojg9 <pablojuniogn2@gmail.com>
2023-06-24 21:21:11 -0300

    create annotation string validator

--- src/main/java/br/com/gntech/webflux/model/request/UserRequest.java
+++ src/main/java/br/com/gntech/webflux/model/request/UserRequest.java
@@ -1,14 +1,17 @@
 package br.com.gntech.webflux.model.request;
 
+import br.com.gntech.webflux.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
 
+        @TrimString
         @Email(message = "invalid email")
         @NotBlank(message = "must not be null or empty")
         String email,
---
+++ src/main/java/br/com/gntech/webflux/validator/TrimStringValidator.java
@@ -0,0 +1,11 @@
+package br.com.gntech.webflux.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class TrimStringValidator implements ConstraintValidator<TrimString, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value == null || value.trim().length() == value.length();
+    }
+}
